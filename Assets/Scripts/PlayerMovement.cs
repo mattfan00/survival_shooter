4ba77bc42fb719256fda7421e956a4a68083e74c
mattfan00 <kungfumo@gmail.com>
2020-08-29 20:08:55 -0400

    animations follow mouse

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Camera camera;
+    public Animator animator;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos - rb.position;
+
+        animator.SetFloat("Horizontal", lookDir.x);
+        animator.SetFloat("Vertical", lookDir.y);
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
